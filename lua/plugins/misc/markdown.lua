11200e7948a9aa312c0c26dea297b5eac5128b87
AstralSheep <109028693+Astral-Sheep@users.noreply.github.com>
2026-05-09 02:37:12 +0200

    feat: add markdown tools

---
+++ lua/plugins/misc/markdown.lua
@@ -0,0 +1,59 @@
+Config.on_very_lazy(function()
+	vim.filetype.add({
+		extension = { mdx = "markdown.mdx" },
+	})
+end)
+
+return {
+	{
+		--- Source ---
+		'mason-org/mason.nvim',
+
+		--- Setup ---
+		opts = { ensure_installed = { 'markdown-toc' } }
+	},
+
+	{
+		--- Source ---
+		'neovim/nvim-lspconfig',
+
+		--- Setup ---
+		opts = {
+			servers = {
+				marksman = {},
+			},
+		},
+	},
+
+	{
+		--- Source --
+		'MeanderingProgrammer/render-markdown.nvim',
+
+		--- Setup ---
+		opts = {
+			code = {
+				sign = false,
+				width = 'block',
+				right_pad = 1,
+			},
+			heading = {
+				sign = false,
+				icons = {},
+			},
+			checkbox = {
+				enabled = false,
+			},
+		},
+		config = function(_, opts)
+			require('render-markdown').setup(opts)
+			Snacks.toggle({
+				name = "Render Markdown",
+				get = require('render-markdown').get,
+				set = require('render-markdown').set,
+			}):map('<leader>um')
+		end,
+
+		--- Lazy loading ---
+		ft = { 'markdown', 'norg', 'rmd', 'org', 'codecompanion' },
+	}
+}
