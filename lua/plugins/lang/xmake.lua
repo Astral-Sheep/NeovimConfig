60c931029f8ce7553ff6ed2a0d2e1edf4ff72e6d
AstralSheep <109028693+Astral-Sheep@users.noreply.github.com>
2026-05-09 04:36:52 +0200

    feat: add xmake support

---
+++ lua/plugins/lang/xmake.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		--- Source ---
+		'Mythos-404/xmake.nvim',
+
+		--- Setup ---
+		opts = {
+			on_save = {
+				reload_project_info = true,
+				lsp_compile_commands = {
+					enable = true,
+					output_dir = "_Binaries",
+				},
+			},
+			lsp = {
+				enable = true,
+			},
+			debuger = { -- I hate that there's a typo here
+				rulus = { 'debug', 'releasedbg' },
+				dap = {
+					name = "Xmake Debug",
+					type = 'codelldb',
+					request = 'launch',
+					cwd = '${workspaceFolder}',
+					console = 'integratedTerminal',
+					stopOnEntry = false,
+					runInTerminal = true,
+				},
+			},
+			dev_debug = false,
+		},
+		config = true,
+
+		--- Lazy loading ---
+		event = 'BufReadPost',
+
+		--- Versioning ---
+		version = '^3',
+	},
+}
